9c29211c1e3a7789689e791efbd01675a793fd50
mrsungura <99541432+mrsungura@users.noreply.github.com>
2023-01-13 20:30:33 +0100

    khjgvjh

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-/* 
-@author SomnathDas
-*/
-
-/*
-Another practice work of mine, using pure CSS and HTML. This is a responsive navbar as you can see the change while resizing the window size. 
-
-uwu)/ until next time
-Last Updated: Jan 01 2022 - 14:00
-*/
 
 const openMenu = (menu) => {
   const ExpansionMenu = document.getElementsByClassName("expansionMenu")[0];
